c928ac93cd3db1005ea705600d2ae072ac1231f3
umv23 <umv23@cs.drexel.edu>
2020-03-10 09:55:50 -0400

    Correction: Fixed validtion bug

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -35,48 +35,50 @@ function createUser(req, res, next) {
         switch (error.code) {
             case 'auth/email-already-in-use':
                 console.log('Email address ' + email + ' already in use.');
-                res.render('register', {
+                return res.render('register', {
                     title: 'CC: Register Failed',
                     error: 'Email address ' + email + ' already in use.',
                     loginFlag: false
                 });
             case 'auth/invalid-email':
                 console.log('Email address ' + email + ' is invalid.');
-                res.render('register', {
+                return res.render('register', {
                     title: 'CC: Register Failed ',
                     error: 'Email address ' + email + ' is invalid.',
                     loginFlag: false
                 });
             case 'auth/operation-not-allowed':
                 console.log('Error during sign up.');
-                res.render('register', {
+                return res.render('register', {
                     title: 'CC: Register Failed',
                     error: 'Error during sign up.',
                     loginFlag: false
                 });
             case 'auth/weak-password':
                 console.log('Password is not strong enough. Add additional characters including special characters and numbers.');
-                res.render('register', {
+                return res.render('register', {
                     title: 'CC: Register Failed',
                     error: 'Password is not strong enough. Add additional characters including special characters and numbers.',
                     loginFlag: false
                 });
             case password != confirmPassword:
                 console.log('Password does not match confirm password! Please try again!');
-                res.render('register', {
+                return res.render('register', {
                     title: 'CC: Register Failed',
                     error: 'Password does not match confirm password! Please try again!',
                     loginFlag: false
                 });
             default:
                 console.log(error.message);
-                res.render('register', {
+                return res.render('register', {
                     title: 'CC: Register Failed',
                     error: error.message,
                     loginFlag: false
                 });
         }
     })
+
+
 }
 
 
